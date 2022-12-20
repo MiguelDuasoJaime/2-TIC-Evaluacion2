09f2fb8c171ffd3f8fb5cb8f2072b8bc58bf41e4
MiguelDuasoJaime <113890420+MiguelDuasoJaime@users.noreply.github.com>
2022-12-20 10:35:03 +0100

    Add files via upload

--- Switch.cpp
+++ Switch.cpp
@@ -2,34 +2,36 @@
 using namespace std;
 int main(){
 	int resp;
-	cout<<"****************"<<endl;
-	cout<<"*     MENU     *"<<endl;
-	cout<<"****************"<<endl;
-	cout<<"1- Volver a mostrar el menu"<<endl;
-	cout<<"2- Aios"<<endl;
-	cout<<"3- Mimir"<<endl;
-	cout<<"ELIGE (po favo): ";
-	cin>>resp;
-	switch(resp){
-		case 1:
-			cout<<"****************"<<endl;
-			cout<<"*     MENU     *"<<endl;
-			cout<<"****************"<<endl;
-			cout<<"1- Volver a mostrar el menu"<<endl;
-			cout<<"2- Aios"<<endl;
-			cout<<"3- Mimir"<<endl;
-			cout<<"ELIGE (po favo): ";
-			cin>>resp;
-			cout<<"Pa que elgiges si se ha acabao?";
+	int cic;
+	cic=1;
+	while(cic==1){
+		cout<<"****************"<<endl;
+		cout<<"*     MENU     *"<<endl;
+		cout<<"****************"<<endl;
+		cout<<"1- Volver a mostrar el menu"<<endl;
+		cout<<"2- Aios"<<endl;
+		cout<<"3- Mimir"<<endl;
+		cout<<"ELIGE (po favo): ";
+		cin>>resp;
+		if (resp!=1 && resp!=2 && resp!=3){
+		resp=4;
+		}
+		switch(resp){
+			case 1:
 			break;
 		case 2:
 			cout<<"Aios";
+			cic=0;
 			break;
 		case 3:
 			cout<<"'";
+			cic=0;
 			break;
 		default:
-			cout<<"a.";
+			cout<<";?"<<endl;
+			cic=0;
+			break;
+		}
 	}
 	return 0;
 }
